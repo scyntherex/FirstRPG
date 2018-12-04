1f8bb45cd024d7c4f7e807a144c91ba99ab14532
Rone <rone.caballero@gmail.com>
2018-12-04 15:35:56 -0800

    Stabalized Player Movement. Used states to switch from idle stance to movement and back.

--- FirstRPG/Assets/Scripts/PlayerController.cs
+++ FirstRPG/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour {
     public Rigidbody2D TestRigidBody;
     public float moveSpeed;
 
+    public Animator myAnim;
+
     // Use this for initialization
     void Start () {
 		
@@ -17,5 +19,16 @@ public class PlayerController : MonoBehaviour {
         TestRigidBody.velocity =
         new Vector2(Input.GetAxisRaw("Horizontal"),
             Input.GetAxisRaw("Vertical")) * moveSpeed;
-	}
+
+        myAnim.SetFloat("moveX", TestRigidBody.velocity.x);
+        myAnim.SetFloat("moveY", TestRigidBody.velocity.y);
+
+        if(Input.GetAxisRaw("Horizontal") == 1 ||
+            Input.GetAxisRaw("Horizontal") == -1 || 
+            Input.GetAxisRaw("Vertical") == 1 ||
+            Input.GetAxisRaw("Vertical") == -1){
+                myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+                myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        }
+    }
 }
