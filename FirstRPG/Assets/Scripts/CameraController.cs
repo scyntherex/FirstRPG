205b5e6cb5fa5f9f8a84b515cf5ce8b4e42e7832
Rone <rone.caballero@gmail.com>
2018-12-06 14:56:30 -0800

    Made script for camera teargeting wherever player goes.

---
+++ FirstRPG/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+        target = PlayerController.instance.transform;
+	}
+
+    // LateUpdate is called once per frame after Update
+    void LateUpdate () {
+        transform.position = 
+            new Vector3(target.position.x, 
+            target.position.y, 
+            transform.position.z);
+	}
+}
