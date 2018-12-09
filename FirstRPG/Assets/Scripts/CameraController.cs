31b746f3bfcd6082d1f81c9f350f368139346155
Rone <rone.caballero@gmail.com>
2018-12-09 15:33:12 -0800

    Added camera function to stop moving within map's bounds.

--- FirstRPG/Assets/Scripts/CameraController.cs
+++ FirstRPG/Assets/Scripts/CameraController.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CameraController : MonoBehaviour {
 
     public Transform target;
 
+    public Tilemap theMap;
+    private Vector3 bottomLeftLimit;
+    private Vector3 topRightLimit;
+
 	// Use this for initialization
 	void Start () {
         target = PlayerController.instance.transform;
+
+        bottomLeftLimit = theMap.localBounds.min;
+        topRightLimit = theMap.localBounds.max;
 	}
 
     // LateUpdate is called once per frame after Update
@@ -17,5 +25,12 @@ public class CameraController : MonoBehaviour {
             new Vector3(target.position.x, 
             target.position.y, 
             transform.position.z);
+
+        //to keep camera inside map bounds.
+        transform.position =
+            new Vector3(Mathf.Clamp(transform.position.x,
+                bottomLeftLimit.x, topRightLimit.x), Mathf.Clamp(
+                transform.position.y, bottomLeftLimit.y, topRightLimit.y),
+                transform.position.z);
 	}
 }
