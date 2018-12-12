548a668c2562a0a32eec7d6102064ce8c85d35c5
Rone <rone.caballero@gmail.com>
2018-12-11 19:34:36 -0800

    Started creating character stats sheet. Mostly involved in calculating exp to level up.

---
+++ FirstRPG/Assets/Scripts/CharStats.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharStats : MonoBehaviour {
+
+    public string charName;
+    public int playerLevel = 1;
+    public int currentEXP;
+    public int[] expToNextLevel;
+    public int maxLevel = 100;
+    public int basEXP = 1000;
+
+
+    public int currentHP;
+    public int maxHP = 100;
+    public int currentMP;
+    public int maxMP = 30;
+    public int strength;
+    public int defence;
+    public int wpnPwr;
+    public int armPwr;
+    public string equippedWpn;
+    public string equippedArmr;
+    public Sprite charImage;
+
+	// Use this for initialization
+	void Start () {
+        expToNextLevel = new int[maxLevel];
+        expToNextLevel[1] = basEXP;
+
+        for(int i = 2; i < expToNextLevel.Length; i++)
+        {
+            expToNextLevel[i] = Mathf.FloorToInt(expToNextLevel[i-1] * 1.05f);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
