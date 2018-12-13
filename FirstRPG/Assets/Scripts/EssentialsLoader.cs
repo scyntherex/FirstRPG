879de75a5dd947e8c310d6649bf91dafb37fd0f3
Rone <rone.caballero@gmail.com>
2018-12-13 14:01:45 -0800

    Started Game Manager to store player stats.

--- FirstRPG/Assets/Scripts/EssentialsLoader.cs
+++ FirstRPG/Assets/Scripts/EssentialsLoader.cs
@@ -6,6 +6,7 @@ public class EssentialsLoader : MonoBehaviour {
 
     public GameObject UIScreen;
     public GameObject player;
+    public GameObject gameMan;
 
 	// Use this for initialization
 	void Start () {
@@ -18,6 +19,11 @@ public class EssentialsLoader : MonoBehaviour {
             PlayerController clone 
                 = Instantiate(player).GetComponent<PlayerController>();
             PlayerController.instance = clone;
+        }
+
+        if(GameManager.instance == null)
+        {
+            Instantiate(gameMan);
         }
     }
 	
