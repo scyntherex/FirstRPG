99e0d263e69a6d0323ceabe6948e6db75639d00a
Rone <rone.caballero@gmail.com>
2018-12-14 15:03:29 -0800

    Characters' stats are updated live during gameplay.

--- FirstRPG/Assets/Scripts/GameMenu.cs
+++ FirstRPG/Assets/Scripts/GameMenu.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameMenu : MonoBehaviour {
 
     public GameObject theMenu;
+
+    private CharStats[] playerStats;
+
+    public Text[] nameText, hpText, mpText, lvlText, expText;
+    public Slider[] expSlider;
+    public Image[] charImage;
+    public GameObject[] charStatHolder;
 
 	// Use this for initialization
 	void Start () {
@@ -23,7 +31,39 @@ public class GameMenu : MonoBehaviour {
             else
             {
                 theMenu.SetActive(true);
+                UpdateMainStats();
                 GameManager.instance.gameMenuOpen = true;
+            }
+        }
+    }
+
+    public void UpdateMainStats()
+    {
+        playerStats = GameManager.instance.playerStats;
+
+        for(int i = 0; i < playerStats.Length; i++)
+        {
+            if(playerStats[i].gameObject.activeInHierarchy)
+            {
+                charStatHolder[i].SetActive(true);
+
+                nameText[i].text = playerStats[i].charName;
+                hpText[i].text = "HP: " + playerStats[i].currentHP +
+                    "/" + playerStats[i].maxHP;
+                mpText[i].text = "MP: " + playerStats[i].currentMP +
+                    "/" + playerStats[i].maxMP;
+                lvlText[i].text = "Lvl: " + playerStats[i].playerLevel;
+                expText[i].text = "" + playerStats[i].currentEXP +
+                    "/" + playerStats[i].expToNextLevel[playerStats[i].
+                    playerLevel];
+                expSlider[i].maxValue = 
+                    playerStats[i].expToNextLevel[playerStats[i].playerLevel];
+                expSlider[i].value = playerStats[i].currentEXP;
+                charImage[i].sprite = playerStats[i].charImage;
+            }
+            else
+            {
+                charStatHolder[i].SetActive(false);
             }
         }
     }
