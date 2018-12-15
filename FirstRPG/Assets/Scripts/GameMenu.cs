d2d45b072ea490dc7d9cc374c6af9173dbc3cd5c
Rone <rone.caballero@gmail.com>
2018-12-15 14:02:03 -0800

    Started menu navigation UI.

--- FirstRPG/Assets/Scripts/GameMenu.cs
+++ FirstRPG/Assets/Scripts/GameMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class GameMenu : MonoBehaviour {
 
     public GameObject theMenu;
+    public GameObject[] windows;
 
     private CharStats[] playerStats;
 
@@ -25,8 +26,9 @@ public class GameMenu : MonoBehaviour {
         {
             if(theMenu.activeInHierarchy)
             {
-                theMenu.SetActive(false);
-                GameManager.instance.gameMenuOpen = false;
+                //theMenu.SetActive(false);
+                //GameManager.instance.gameMenuOpen = false;
+                CloseMenu();
             }
             else
             {
@@ -66,5 +68,31 @@ public class GameMenu : MonoBehaviour {
                 charStatHolder[i].SetActive(false);
             }
         }
+    }
+
+    public void ToggleWindow(int windowNumber)
+    {
+        for(int i = 0; i < windows.Length; i++)
+        {
+            if(i == windowNumber)
+            {
+                windows[i].SetActive(!windows[i].activeInHierarchy);
+            }
+            else
+            {
+                windows[i].SetActive(false);
+            }
+        }
+    }
+
+    public void CloseMenu()
+    {
+        for(int i = 0; i < windows.Length; i++)
+        {
+            windows[i].SetActive(false);
+        }
+
+        theMenu.SetActive(false);
+        GameManager.instance.gameMenuOpen = false;
     }
 }
