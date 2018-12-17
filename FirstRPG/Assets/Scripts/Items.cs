f52f889fc168171e8b8182362b964402a32ba523
Rone <rone.caballero@gmail.com>
2018-12-17 13:40:50 -0800

    Created Basic Items.

---
+++ FirstRPG/Assets/Scripts/Items.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items : MonoBehaviour {
+    [Header("Item Type")]
+    public bool isItem;
+    public bool isWeapon;
+    public bool isArmour;
+
+    [Header("Item Appearance")]
+    public string itemName;
+    public string itemDescription;
+
+    public int price;
+    public Sprite itemSprite;
+
+    [Header("Item Details")]
+    public int amountToChange;
+    public bool affectHP, affectMP, affectStr;
+
+    [Header("Item Weapon/Armor Details")]
+    public int weaponStrength;
+
+    public int armorStrength; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
