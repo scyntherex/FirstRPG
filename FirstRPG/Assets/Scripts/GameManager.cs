715fa15cb82a1652dedd4f91e3be53247f5fcca0
Rone <rone.caballero@gmail.com>
2018-12-26 18:07:56 -0800

    Saving game features implemented.

--- FirstRPG/Assets/Scripts/GameManager.cs
+++ FirstRPG/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
@@ -47,6 +48,10 @@ public class GameManager : MonoBehaviour {
             RemoveItem("Mana Potion");
             RemoveItem("Beep");
         }*/
+        if (Input.GetKeyDown(KeyCode.O))
+            SaveData();
+        if (Input.GetKeyDown(KeyCode.P))
+            LoadData();
     }
 
     public Items GetItemDetails(string itemToGrab)
@@ -161,5 +166,121 @@ public class GameManager : MonoBehaviour {
         {
             Debug.LogError("Couldn't find " + itemToRemove);
         }
+    }
+
+    public void SaveData()
+    {
+        PlayerPrefs.SetString("Current_Scene", 
+            SceneManager.GetActiveScene().name);
+        PlayerPrefs.SetFloat("Player_Position_x",
+            PlayerController.instance.transform.position.x);
+        PlayerPrefs.SetFloat("Player_Position_y",
+            PlayerController.instance.transform.position.y);
+        PlayerPrefs.SetFloat("Player_Position_z",
+            PlayerController.instance.transform.position.z);
+
+        //save char info
+        for(int i = 0; i < playerStats.Length; i++)
+        {
+            if(playerStats[i].gameObject.activeInHierarchy)
+            {
+                PlayerPrefs.SetInt("Player_" + playerStats[i].charName +
+                    "_active", 1);
+            }
+            else
+            {
+                PlayerPrefs.SetInt("Player_" + playerStats[i].charName +
+                    "_active", 0);
+            }
+
+            PlayerPrefs.SetInt("Player_" + playerStats[i].charName + 
+                "_Level", playerStats[i].playerLevel);
+            PlayerPrefs.SetInt("Player_" + playerStats[i].charName +
+                "_CurrentEXP", playerStats[i].currentEXP);
+            PlayerPrefs.SetInt("Player_" + playerStats[i].charName +
+                "_CurrentHP", playerStats[i].currentHP);
+            PlayerPrefs.SetInt("Player_" + playerStats[i].charName +
+                "_maxHP", playerStats[i].maxHP);
+            PlayerPrefs.SetInt("Player_" + playerStats[i].charName +
+                "_CurrentMP", playerStats[i].currentMP);
+            PlayerPrefs.SetInt("Player_" + playerStats[i].charName +
+                "_maxMP", playerStats[i].maxMP);
+            PlayerPrefs.SetInt("Player_" + playerStats[i].charName +
+                "_Strength", playerStats[i].strength);
+            PlayerPrefs.SetInt("Player_" + playerStats[i].charName +
+                "_Defense", playerStats[i].defence);
+            PlayerPrefs.SetInt("Player_" + playerStats[i].charName +
+                "_WpnPwr", playerStats[i].wpnPwr);
+            PlayerPrefs.SetInt("Player_" + playerStats[i].charName +
+                "_ArmPwr", playerStats[i].armPwr);
+            PlayerPrefs.SetString("Player_" + playerStats[i].charName +
+                "_EquippedWpn", playerStats[i].equippedWpn);
+            PlayerPrefs.SetString("Player_" + playerStats[i].charName +
+                "_EquippedArm", playerStats[i].equippedArmr);
+        }
+
+        //store inventory info
+        for (int i = 0; i < itemsHeld.Length; i++)
+        {
+            PlayerPrefs.SetString("ItemInInventory_" + i, itemsHeld[i]);
+            PlayerPrefs.SetInt("ItemAmount_" + i, numberOfItems[i]);
+        }
+
+        //store gold info
+        PlayerPrefs.SetInt("Gold", currentGold);
+    }
+
+    public void LoadData()
+    {
+        PlayerController.instance.transform.position = new Vector3(
+            PlayerPrefs.GetFloat("Player_Position_x"),
+            PlayerPrefs.GetFloat("Player_Position_y"),
+            PlayerPrefs.GetFloat("Player_Position_z"));
+
+        for (int i = 0; i < playerStats.Length; i++)
+        {
+            if(PlayerPrefs.GetInt("Player_" + playerStats[i].charName + 
+                "_active") == 0)
+            {
+                playerStats[i].gameObject.SetActive(false);
+            }
+            else
+            {
+                playerStats[i].gameObject.SetActive(true);
+            }
+
+            playerStats[i].playerLevel = PlayerPrefs.GetInt("Player_" + 
+                playerStats[i].charName + "_Level");
+            playerStats[i].currentEXP = PlayerPrefs.GetInt("Player_" + 
+                playerStats[i].charName + "_CurrentEXP");
+            playerStats[i].currentHP = PlayerPrefs.GetInt("Player_" + 
+                playerStats[i].charName + "_CurrentHP");
+            playerStats[i].maxHP = PlayerPrefs.GetInt("Player_" + 
+                playerStats[i].charName + "_maxHP");
+            playerStats[i].currentMP = PlayerPrefs.GetInt("Player_" + 
+                playerStats[i].charName + "_CurrentMP");
+            playerStats[i].maxMP = PlayerPrefs.GetInt("Player_" + 
+                playerStats[i].charName + "_maxMP");
+            playerStats[i].strength = PlayerPrefs.GetInt("Player_" + 
+                playerStats[i].charName + "_Strength");
+            playerStats[i].defence = PlayerPrefs.GetInt("Player_" + 
+                playerStats[i].charName + "_Defense");
+            playerStats[i].wpnPwr = PlayerPrefs.GetInt("Player_" + 
+                playerStats[i].charName + "_WpnPwr");
+            playerStats[i].armPwr = PlayerPrefs.GetInt("Player_" + 
+                playerStats[i].charName + "_ArmPwr");
+            playerStats[i].equippedWpn = PlayerPrefs.GetString("Player_" + 
+                playerStats[i].charName + "_EquippedWpn");
+            playerStats[i].equippedArmr = PlayerPrefs.GetString("Player_" + 
+                playerStats[i].charName + "_EquippedArm");
+        }
+
+        for (int i = 0; i < itemsHeld.Length; i++)
+        {
+            itemsHeld[i] = PlayerPrefs.GetString("ItemInInventory_" + i);
+            numberOfItems[i] = PlayerPrefs.GetInt("ItemAmount_" + i);
+        }
+
+        currentGold = PlayerPrefs.GetInt("Gold", currentGold);
     }
 }
--- FirstRPG/Assets/Scripts/GameMenu.cs
+++ FirstRPG/Assets/Scripts/GameMenu.cs
@@ -250,4 +250,10 @@ public class GameMenu : MonoBehaviour {
         activeItem.Use(selectChar);
         CloseItemCharChoice();
     }
+
+    public void SaveGame()
+    {
+        GameManager.instance.SaveData();
+        QuestManager.instance.SaveQuestData();
+    }
 }
