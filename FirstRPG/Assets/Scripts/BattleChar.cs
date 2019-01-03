6e8572d948506799c6b74e1294cc5743b7e54ade
Rone <rone.caballero@gmail.com>
2019-01-02 16:57:32 -0800

    Added Character Positions and created first enemies.

---
+++ FirstRPG/Assets/Scripts/BattleChar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleChar : MonoBehaviour {
+
+    public bool isPlayer;
+    public string[] movesAvailable;
+
+    public string charName;
+    public int currentHP, maxHP, currentMP, maxMP, strength, defence,
+        wpnPwr, armrPwr;
+    public bool hasDied;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
