b27a645a5e8ddf33a148f3e5f0cb152de9d4de33
Rone <rone.caballero@gmail.com>
2019-01-03 09:25:48 -0800

    Implemented transition between worldview scene and battle scene.

--- FirstRPG/Assets/Scripts/GameManager.cs
+++ FirstRPG/Assets/Scripts/GameManager.cs
@@ -10,7 +10,7 @@ public class GameManager : MonoBehaviour {
     public CharStats[] playerStats;
 
     public bool gameMenuOpen, dialogActive, fadingBetweenAreas,
-        shopActive;
+        shopActive, battleActive;
 
     public string[] itemsHeld;
     public int[] numberOfItems;
@@ -30,7 +30,7 @@ public class GameManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(gameMenuOpen || dialogActive || fadingBetweenAreas
-            || shopActive)
+            || shopActive || battleActive)
         {
             PlayerController.instance.canMove = false;
         }
