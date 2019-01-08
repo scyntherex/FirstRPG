546e8d6b6858f881cbab818a5c38afa57490b8a6
Rone <rone.caballero@gmail.com>
2019-01-07 16:22:28 -0800

    Designed Battle rewards screen.

---
+++ FirstRPG/Assets/Scripts/BattleRewards.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleRewards : MonoBehaviour {
+
+    public static BattleRewards instance;
+
+    public Text xpText, itemText;
+    public GameObject rewardScreen;
+
+    public string[] rewardItems;
+    public int xpEarned;
+
+	// Use this for initialization
+	void Start () {
+        instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Y))
+        {
+            OpenRewardsScreen(54, new string[] { "Iron Armor", "Wooden Sword" });
+        }
+    }
+
+    public void OpenRewardsScreen(int xp, string[] rewards)
+    {
+        xpEarned = xp;
+        rewardItems = rewards;
+
+        xpText.text = "Players earned " + xpEarned + " exp points.";
+        itemText.text = "";
+
+        for(int i = 0; i < rewardItems.Length; i++)
+        {
+            itemText.text += rewards[i] + "\n";
+        }
+
+        rewardScreen.SetActive(true);
+    }
+
+    public void CloseRewardsScreen()
+    {
+
+    }
+}
