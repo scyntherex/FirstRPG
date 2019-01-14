1027af3790473374e21f3bde58f19f3ccd535e35
Rone <rone.caballero@gmail.com>
2019-01-14 13:05:40 -0800

    Added Autodialog feature. Finished ground design of Draco City.

---
+++ FirstRPG/Assets/Scripts/AutoDialog.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDialog : MonoBehaviour {
+
+    private bool autoActive;
+    public string[] dialogLines;
+    public bool isPerson;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (autoActive)
+        {
+            StartCoroutine(StartDialog());
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+            autoActive = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            autoActive = false;
+        }
+    }
+
+    public IEnumerator StartDialog()
+    {
+        PlayerController.instance.canMove = false;
+        yield return new WaitForSeconds(1.0f);
+        DialogManager.instance.showDialog(dialogLines, isPerson);
+        PlayerController.instance.canMove = true;
+        Destroy(gameObject);
+    }
+}
